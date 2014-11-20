23e7ad7a0c972e719b9ab95b7ffede10bbb134df
mohayonao <mohayonao@gmail.com>
2014-11-20 19:28:49 +0900

    fix window exports

--- src/index.js
+++ src/index.js
@@ -4,4 +4,9 @@ var MMLEmitter = require("./mml-emitter");
 
 MMLEmitter.version = "0.2.6";
 
+/* istanbul ignore next */
+if (typeof global.window !== "undefined") {
+  global.window.MMLEmitter = MMLEmitter;
+}
+
 module.exports = MMLEmitter;
