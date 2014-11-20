5c9d07a272a32564bf39aaf5c4da0aa0e6eb289c
mohayonao <mohayonao@gmail.com>
2014-11-20 12:51:28 +0900

    fix gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,34 +1,35 @@
 "use strict";
 
-var gulp      = require("gulp");
+var gulp = require("gulp");
 var browserify = require("browserify");
 var source = require("vinyl-source-stream");
 var buffer = require("vinyl-buffer");
-var istanbul  = require("gulp-istanbul");
-var jshint    = require("gulp-jshint");
-var mocha     = require("gulp-mocha");
-var rename    = require("gulp-rename");
-var uglify    = require("gulp-uglify");
+var istanbul = require("gulp-istanbul");
+var jshint = require("gulp-jshint");
+var mocha = require("gulp-mocha");
+var rename = require("gulp-rename");
+var uglify = require("gulp-uglify");
 
 gulp.task("lint", function() {
-  gulp.src([ "gulpfile.js", "src/**/*.js" ])
+  return gulp.src([ "gulpfile.js", "src/**/*.js" ])
     .pipe(jshint(".jshintrc"))
     .pipe(jshint.reporter(require("jshint-stylish")))
     .pipe(jshint.reporter("fail"));
 });
 
 gulp.task("test", function() {
-  gulp.src("test/**/*.js")
+  return gulp.src("test/**/*.js")
     .pipe(mocha());
 });
 
-gulp.task("cover", function() {
+gulp.task("cover", function(cb) {
   gulp.src("src/**/*.js")
     .pipe(istanbul())
     .on("finish", function() {
       return gulp.src("test/**/*.js")
         .pipe(mocha())
-        .pipe(istanbul.writeReports("coverage"));
+        .pipe(istanbul.writeReports("coverage"))
+        .on("end", cb);
     });
 });
 
