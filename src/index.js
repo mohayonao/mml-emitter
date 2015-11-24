f0c1d290abc6216eb0c2255f4e6dbf3f73379801
mohayonao <mohayonao@gmail.com>
2015-11-24 09:44:24 +0900

    re-init

--- src/index.js
+++ src/index.js
@@ -1,12 +1,1 @@
-"use strict";
-
-var MMLEmitter = require("./mml-emitter");
-
-MMLEmitter.version = "0.2.8";
-
-/* istanbul ignore next */
-if (typeof global.window !== "undefined") {
-  global.window.MMLEmitter = MMLEmitter;
-}
-
-module.exports = MMLEmitter;
+export default {};
--- test/bootstrap/bootstrap.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-global.chai   = require("chai");
-global.expect = global.chai.expect;
-
-require("web-audio-test-api");
-
-global.MMLEmitter = require("../../");
